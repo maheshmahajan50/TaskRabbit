4f327026fe6eb962c4fc59abce4afe8fa53e4d7b
mahesh mahajan <mahajanmahesh50@gmail.com>
2020-02-19 15:57:59 +0530

    Add sessions functionality per user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,28 +1,28 @@
 class TasksController < ApplicationController
+  before_action :find_task,    only: [:edit, :show, :update, :destroy]
 
   def index
     if params[:category].blank?
-      @tasks = Task.all.order("created_at DESC")
+      @tasks = current_user.tasks.all.order("created_at DESC")
     else
       @category_id = Category.find_by(name: params[:category]).id
-      @tasks = Task.where(category_id: @category_id).order("created_at DESC")
+      @tasks = current_user.tasks.where(category_id: @category_id).order("created_at DESC")
     end      
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build
     @categories = Category.all.map{ |cat| [cat.name, cat.id] }
   end
     
   def edit
-    @task = Task.find(params[:id])
     @categories = Category.all.map{ |cat| [cat.name, cat.id] }
 
   end  
   
   def create
-    @task = Task.new(allowed_params)
-    @task.user = User.first
+    @task = current_user.tasks.build(allowed_params)
+    @task.user = current_user
     @task.category_id = params[:category_id]
     if @task.save!
       redirect_to tasks_path
@@ -32,7 +32,6 @@ class TasksController < ApplicationController
   end
 
   def update
-    @task = Task.find(params[:id])
     @categories = Category.all.map{ |cat| [cat.name, cat.id] }
 
     if @task.update_attributes!(allowed_params)
@@ -43,11 +42,10 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
+    
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -56,6 +54,10 @@ class TasksController < ApplicationController
 
   def allowed_params
     params.require(:task).permit(:title, :description, :company_name, :location, :estimated_date, :category_id)
+  end 
+
+  def find_task
+    @task = current_user.tasks.find(params[:id])
   end  
 
 end
